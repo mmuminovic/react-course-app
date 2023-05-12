4b90275689e54a802834c4351ec0a7448e213dad
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2023-05-12 19:59:43 +0200

    classname instead of class

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -5,27 +5,27 @@ import PlusImg from "./slika/plus.png";
 class App extends React.Component {
   render() {
     return (
-      <div class="container">
-        <div class="todo__box">
-          <div class="title">
-            <h2 class="h__todo">Todo App</h2>
+      <div className="container">
+        <div className="todo__box">
+          <div className="title">
+            <h2 className="h__todo">Todo App</h2>
           </div>
-          <div class="form">
+          <div className="form">
             <input id="taskName" type="add" placeholder="Add your new todo" />
-            <button id="btnn1" class="btnn1">
-              <img src={PlusImg} class="img" alt="" />
+            <button id="btnn1" className="btnn1">
+              <img src={PlusImg} className="img" alt="" />
             </button>
           </div>
-          <div id="list" class="d-grid gap-2 col-6 button">
-            {/* <button class="list-items">how to make a game</button> */}
-            {/* <button class="list-items">keyword researching</button> */}
-            {/* <button class="list-items">ui ux desing for app</button> */}
+          <div id="list" className="d-grid gap-2 col-6 button">
+            {/* <button className="list-items">how to make a game</button> */}
+            {/* <button className="list-items">keyword researching</button> */}
+            {/* <button className="list-items">ui ux desing for app</button> */}
           </div>
-          <div class="footerr">
-            <p class="p">
+          <div className="footerr">
+            <p className="p">
               You have <span id="numberOfItems">0</span> pending tasks
             </p>
-            <button id="btnn2" class="btnn2">
+            <button id="btnn2" className="btnn2">
               Clear All
             </button>
           </div>
