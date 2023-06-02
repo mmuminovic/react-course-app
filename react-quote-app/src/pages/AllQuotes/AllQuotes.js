fd927e75998e64c189d83267bc4ee6f1007d4e2d
Muhamed Muminović <muminovic.muhamed01@gmail.com>
2023-06-02 20:28:31 +0200

    get all quotes

--- react-quote-app/src/pages/AllQuotes/AllQuotes.js
+++ react-quote-app/src/pages/AllQuotes/AllQuotes.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./AllQuotes.css";
 
 function AllQuotes() {
+  const [quotes, setQuotes] = useState([]);
+
+  useEffect(() => {
+    fetch("https://js-course-server.onrender.com/quotes/get-all-quotes")
+      .then((res) => res.json())
+      .then((data) => {
+        setQuotes(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(quotes);
+
   return <div className="all-quotes">AllQuotes</div>;
 }
 
